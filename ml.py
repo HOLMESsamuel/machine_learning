7590dc38bbe2fab6b96e55955d4f8971250a4bca
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2019-11-10 10:15:59 +0100

    Update ml.py
    implementation of the Neural_netwok class using the layer class

--- ml.py
+++ ml.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def sigmoid(x):
+	return (1/(1+np.exp(-x)))
+
 class Layer:
   def __init__(self, size, previous_size):
     self.size = size
@@ -8,8 +11,21 @@ class Layer:
     self.biases = np.ones((size,1))
     self.values = np.zeros((size,1))
 
-def sigmoid(x):
-	return (1/(1+np.exp(-x)))
+class Neural_network:
+  def __init__(self, dimensions):
+    layers = []
+    input_layer = Layer(dimensions[0], 0)
+    layers.append(input_layer)
+    for i in range(1, len(dimensions)):
+      new_layer = Layer(dimensions[i], dimensions[i-1])
+      layers.append(new_layer)
+    self.layers = layers
+
+  def enter_input(self, input_values):
+    for i in range(len(input_values)):
+      self.layers[0].values[i][0] = input_values[i]
+
+
 
 
 def next_layer(previous_layer, current_layer):
@@ -17,25 +33,18 @@ def next_layer(previous_layer, current_layer):
   current_layer.values = sigmoid(current_layer.values)
 
 
-def output(layers):
-	steps = len(layers)
-	for i in range(1, steps):
-		next_layer(layers[i-1], layers[i])
-	return layers[steps-1].values
+def output(input, network):
+  steps = len(network.layers)
+  network.enter_input(input)
+  for i in range(1, steps):
+    next_layer(network.layers[i-1], network.layers[i])
+  return network.layers[steps-1].values
 
-def create_layers(input_values, format):
-  layers = []
-  input_layer = Layer(len(input_values), 0)
-  for i in range(len(input_values)):
-    input_layer.values[i][0] = input_values[i]
-  layers.append(input_layer)
-  for i in range(1, len(format)):
-    new_layer = Layer(format[i], format[i-1])
-    layers.append(new_layer)
-  return layers
+  
 
+network = Neural_network([2, 2, 1])
 
-print(output(create_layers([1, 1], [2, 2, 1])))
+print(output([1, 1], network))
 	
 
 
