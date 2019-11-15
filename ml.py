88c2661f08362daf4871cbb20f35638f091b7eb4
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2019-11-15 20:44:51 +0100

    Update ml.py

--- ml.py
+++ ml.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(x):
 	return (1/(1+np.exp(-x)))
 
+def d_sigmoid(x):
+  return x
+
 class Layer:
   def __init__(self, size, previous_size):
     self.size = size
@@ -13,6 +16,7 @@ class Layer:
 
 class Neural_network:
   def __init__(self, dimensions):
+    self.learning_rate = 0.1
     layers = []
     input_layer = Layer(dimensions[0], 0)
     layers.append(input_layer)
@@ -33,6 +37,14 @@ def next_layer(previous_layer, current_layer):
   current_layer.values = sigmoid(current_layer.values)
 
 
+def feedforward(input, network):
+  steps = len(network.layers)
+  network.enter_input(input)
+  for i in range(1, steps):
+    next_layer(network.layers[i-1], network.layers[i])
+  return network.layers
+
+
 def output(input, network):
   steps = len(network.layers)
   network.enter_input(input)
@@ -50,13 +62,25 @@ def error(input, network, wanted_output):
     errors.append(error)
   return errors
 
+def delta(input, network, wanted_output):
+  fed_network = feedforward(input, network)
+  errors = error(input, network, wanted_output)
+  for i in range(len(network.layers), 1, -1):
+    gradients = network.learning_rate * np.dot(d_sigmoid(fed_network[i-1]), errors[i-2])
+    print(gradients)
+
+def train(input, network, wanted_output):
+  return "ok"
+
   
 
 network = Neural_network([2, 2, 1])
 
-print(output([1, 1], network))
+#print(output([1, 1], network))
 
-print(error([1, 1], network, [[1]]))
+#print(error([1, 1], network, [[1]]))
+
+delta([1, 1], network, [[1]])
 	
 
 
