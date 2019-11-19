53788c4a64cf0b38ae85628fa22ec193992cb2bf
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2019-11-19 13:42:19 +0100

    implementation of the draw function that will allow to visualize the nn

--- ml.py
+++ ml.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 def sigmoid(x):
 	return (1/(1+np.exp(-x)))
@@ -17,6 +18,7 @@ class Layer:
 
 class Neural_network:
   def __init__(self, dimensions):
+    self.dimensions = dimensions
     self.learning_rate = 0.1
     layers = []
     input_layer = Layer(dimensions[0], 0)
@@ -29,6 +31,42 @@ class Neural_network:
   def display(self):
     for i in range(len(self.layers)):
       print(self.layers[i].weights, self.layers[i].values, self.layers[i].biases)
+  
+  def draw_neural_network(self, left, right, bottom, top):
+    '''
+    :parameters:
+        - left : float
+            The center of the leftmost node(s) will be placed here
+        - right : float
+            The center of the rightmost node(s) will be placed here
+        - bottom : float
+            The center of the bottommost node(s) will be placed here
+        - top : float
+            The center of the topmost node(s) will be placed here
+    '''
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.gca()
+    ax.axis('off')
+    n_layers = len(self.dimensions)
+    v_spacing = (top - bottom)/float(max(self.dimensions))
+    h_spacing = (right - left)/float(len(self.dimensions) - 1)
+    # Nodes
+    for n, layer_size in enumerate(self.dimensions):
+        layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
+        for m in xrange(layer_size):
+            circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4.,
+                                color='w', ec='k', zorder=4)
+            ax.add_artist(circle)
+    # Edges
+    for n, (layer_size_a, layer_size_b) in enumerate(zip(self.dimensions[:-1], self.dimensions[1:])):
+        layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
+        layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
+        for m in xrange(layer_size_a):
+            for o in xrange(layer_size_b):
+                line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
+                                  [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
+                ax.add_artist(line)
+    fig.savefig('nn.png')
 
 
 
@@ -60,7 +98,7 @@ def output(input, network):
 def error(input, network, wanted_output):
   errors = []
   output_result = output(input, network)
-  error = np.subtract(wanted_output, output_result))
+  error = np.subtract(wanted_output, output_result)
   errors.append(error)
   for i in range(1, len(network.layers)-1):
     error = np.dot(network.layers[len(network.layers)-i].weights.transpose(), error)
@@ -94,7 +132,7 @@ def train(input, network, wanted_output):
 
   
 
-network = Neural_network([2, 2, 1])
+network = Neural_network([2, 4, 1])
 
 
 
@@ -105,7 +143,7 @@ for i in range(50000):
   choice = random.choice(training_set)
   train(choice[0], network, choice[1])
 
-
+network.draw_neural_network(.1, .9, .1, .9)
 print(output(np.array([[1], [1]]), network))
 print(output(np.array([[0], [0]]), network))
 print(output(np.array([[0], [1]]), network))
